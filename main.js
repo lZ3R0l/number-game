498e4b1f48fee454020f6dcf4852e50e92e00864
lZ3R0l <galaxyeye.1999@gmail.com>
2025-03-26 16:42:04 +0100

    added ready alert

--- main.js
+++ main.js
@@ -3,5 +3,6 @@ import { guessNumber } from './guess-number.js';
 let choice = prompt('Do you want to play a game? (y/n)');
 
 if (choice.toLowerCase() == 'y') {
+    alert('Ready!');
     guessNumber();
 }
